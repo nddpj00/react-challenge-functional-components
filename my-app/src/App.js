5e212ea9ce06d6eedf4d661ebc412689c6a53253
nddpj00 <danieljones625@btinternet.com>
2023-03-27 19:07:13 +0000

    points css to component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className={css.App}>
-      {/* Add your components here */}
-    </div>
-  );
-}
+  <div className={css.App}>
+    <Sidebar/>
+  </div>
+);
+};
 
 export default App;
