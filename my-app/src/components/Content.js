d99e22fc26d1df38fe96aed5e000b719215f2e6e
nddpj00 <danieljones625@btinternet.com>
2023-03-30 17:46:15 +0000

    lifecycle challenge adding Loader component with conditional render and timer

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 
 export class Content extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoaded: false
   }
+}
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true,
+      })
+    }, 2000)
+  }
+
   render() {
     return (
       <div className={css.Content}>
@@ -17,8 +30,11 @@ export class Content extends Component {
         </div>
 
         <div className={css.SearchResults}>
+          {this.state.isLoaded ? (
+          <PostItem savedPosts={savedPosts}/>)
+          :
+          (<Loader />)}
 
-          <PostItem savedPosts={savedPosts}/>
         </div>
       </div>
     );
